75f2779348fc83cea346b459478c69c31fcd319e
Carlo Pezzuto <45166937+carlopezzuto@users.noreply.github.com>
2025-07-21 15:32:41 +0200

    test: parametrize getSearchUrl platforms

--- popup.test.js
+++ popup.test.js
@@ -1,37 +1,17 @@
 const { getSearchUrl } = require('./popup');
 
 describe('getSearchUrl', () => {
-  test('generates Google search URL', () => {
-    const url = getSearchUrl('Google', 'hello world');
-    expect(url).toBe('https://www.google.com/search?q=hello%20world');
-  });
+  const cases = [
+    ['Google', 'https://www.google.com/search?q=hello%20world'],
+    ['Linkedin', 'https://www.linkedin.com/search/results/people/?keywords=hello%20world'],
+    ['Github', 'https://github.com/search?type=Users&q=hello%20world'],
+    ['MobyGames', 'https://www.mobygames.com/search/quick?q=hello%20world'],
+    ['ArtStation', 'https://www.artstation.com/search/artists?sort_by=followers&query=hello%20world'],
+    ['Behance', 'https://www.behance.net/search/users?search=hello%20world'],
+  ];
 
-  test('generates Linkedin search URL', () => {
-    const url = getSearchUrl('Linkedin', 'hello world');
-    expect(url).toBe(
-      'https://www.linkedin.com/search/results/people/?keywords=hello%20world'
-    );
-  });
-
-  test('generates Github search URL', () => {
-    const url = getSearchUrl('Github', 'hello world');
-    expect(url).toBe('https://github.com/search?type=Users&q=hello%20world');
-  });
-
-  test('generates MobyGames search URL', () => {
-    const url = getSearchUrl('MobyGames', 'hello world');
-    expect(url).toBe('https://www.mobygames.com/search/quick?q=hello%20world');
-  });
-
-  test('generates ArtStation search URL', () => {
-    const url = getSearchUrl('ArtStation', 'hello world');
-    expect(url).toBe(
-      'https://www.artstation.com/search/artists?sort_by=followers&query=hello%20world'
-    );
-  });
-
-  test('generates Behance search URL', () => {
-    const url = getSearchUrl('Behance', 'hello world');
-    expect(url).toBe('https://www.behance.net/search/users?search=hello%20world');
+  test.each(cases)('generates %s search URL', (platform, expected) => {
+    const url = getSearchUrl(platform, 'hello world');
+    expect(url).toBe(expected);
   });
 });
